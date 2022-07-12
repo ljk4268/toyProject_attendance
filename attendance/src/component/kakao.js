45dd72f7c37711938aecb72ac99c0e55539144ec
ljk4268 <ljk4268@kakao.com>
2022-07-12 16:18:24 +0900

    로그인 구현하는 중

--- attendance/src/component/kakao.js
+++ attendance/src/component/kakao.js
@@ -1,5 +1,23 @@
-function Kakao(props){
-  props.setMode('Kakao')
+import axios from 'axios';
+import { useEffect } from 'react';
+
+function Kakao(){
+  
+  const href = window.location.href;
+  let url = new URL(href);
+  let urlParams = url.searchParams;
+  let code = urlParams.get('code');
+  console.log(code)
+
+  
+    try{
+      axios.get(`http://3.36.247.2/app/kakao/oauth?code=${code}`)
+      .then((reponse)=>{console.log(reponse)})
+    } catch(error){
+      console.error(error);
+    }
+  
+
   return(
 
     <h1>안녕하세요</h1>
