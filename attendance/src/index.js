e454f1649f61e3dd2f732019ae469642fbf6d92d
ljk4268 <ljk4268@kakao.com>
2022-08-16 09:47:28 +0900

    주소변경

--- attendance/src/index.js
+++ attendance/src/index.js
@@ -15,7 +15,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Provider store={store}>
     <PersistGate loading={null} persistor={persistor}>
-      <BrowserRouter basename="/attendance-pages">
+      <BrowserRouter basename="/toyProject_attendance">
           <App />
       </BrowserRouter>
     </PersistGate>
